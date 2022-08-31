eda3c77499c921181f917905d3322cc983109b85
Anida04 <111961923+Anida04@users.noreply.github.com>
2022-08-31 14:08:20 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+cout << " Nama : Rahma Tri Anida " << endl;
+cout << " Kelas : X PPLG 1" << endl;
+cout<< "=========" << endl ; 
+cout<< "Angka = 10"<< endl ;
+cout<< "Angka = 5" << endl ;
+cout << " Hasil:" << 10 + 5 << endl ;
+}
